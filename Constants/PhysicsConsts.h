3e08da848e34f7d5f7f0aac2e957a771acc7bee2
AliClark2023 <1102533@uad.ac.uk>
2026-04-28 10:57:49 +0100

    Cleaned up physic Consts

--- Constants/PhysicsConsts.h
+++ Constants/PhysicsConsts.h
@@ -1,3 +1,5 @@
 #pragma once
-constexpr float PLAYER_V_SCALE{ 150.f };
-constexpr sf::Vector2f UP{ 0.0f, -1.f};
+// constants used by player physics to adjust behaviour
+
+inline constexpr float PLAYER_V_SCALE{ 150.f };
+inline constexpr sf::Vector2f UP{ 0.0f, -1.f};
